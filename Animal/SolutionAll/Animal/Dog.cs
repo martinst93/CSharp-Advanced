7af9a1dbed1e98b749d0283274bbfad776cfdd2f
martinst93 <stojanov003@gmail.com>
2021-06-01 21:31:24 +0200

    Serialization & Deserealization

---
+++ Animal/SolutionAll/Animal/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Animal
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public static void CreateDogMethod(string name, int age, string color)
+        {
+            string path = @"E:\vezbi programiranje\C# Advanced\CSharp-Advanced\Animal\SolutionAll\Animal.json";
+            
+            Dog sparky = new Dog()
+            {
+                Name = name,
+                Age = age,
+                Color = color
+            };
+
+            string dogString = JsonConvert.SerializeObject(sparky);
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(dogString);
+            }
+
+            Dog dog = JsonConvert.DeserializeObject<Dog>(dogString);
+            Console.WriteLine($"Name: {dog.Name},\nAge: {dog.Age},\nColor: {dog.Color}");
+                
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string result = sr.ReadToEnd();
+                Console.WriteLine(new string('-', 60));
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Animal/SolutionAll/Animal/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Animal
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a name for the dog.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter age of the dog.");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the color of the dog.");
+            string color = Console.ReadLine();
+
+            Dog.CreateDogMethod(name, age, color);
+        }
+    }
+}
